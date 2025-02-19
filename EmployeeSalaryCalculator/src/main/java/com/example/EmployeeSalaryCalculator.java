5bc599b112993797bb92e2f7528dd8b047b190bc
thereelslimshady <jacynthe_tessier@outlook.com>
2025-02-19 17:29:29 -0500

    final commit

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -54,6 +54,7 @@ public class EmployeeSalaryCalculator
 			3. Do not change existing class names, interface names, method signatures, or attributes.
 			4. Follow standard Java naming conventions for all code that you write.
 		*/
+
 				if(HW <= standardWorkingHours)
 				{
 					//YOUR CODE STARTS HERE
